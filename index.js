19d712bfdf160d5b6437918ec92b00922b01d15a
Abhishek <choudharyabhishek242004@gmail.com>
2026-07-13 22:31:19 +0530

    fixed some errors/bugs

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const express=require('express');
+const express = require('express');
 const path = require('path');
-const app=express();
-const fs=require('fs')
-app.set("view engine","ejs");
+const app = express();
+const fs = require('fs');
+
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',function(req,res){
-    fs.readdir(`./files`,function(err,files){
-        res.render("index",{files:files});
-    })
-})
-
-app.get('/files/:filename',function(req,res){
-    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
-        console.log(filedata);
-        res.render(show,{filename:req.params.filename,filedata:filedata});
-    })
-})
-
-app.post('/create', function(req, res){
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
-        res.redirect('/');
-    }); 
+app.get('/', function (req, res) {
+    fs.readdir(path.join(__dirname, "files"), function (err, files) {
+        if (err) return res.status(500).send("Error reading files");
+        res.render("index", { files: files });
+    });
 });
-app.listen(3000);
+
+app.get('/files/:filename', function (req, res) {
+    fs.readFile(path.join(__dirname, "files", req.params.filename), "utf-8", function (err, filedata) {
+        if (err) return res.status(500).send("Error reading file");
+        res.render("show", { filename: req.params.filename, filedata: filedata });
+    });
+});
+
+app.post('/create', function (req, res) {
+    fs.writeFile(
+        path.join(__dirname, "files", `${req.body.title.split(' ').join('')}.txt`),
+        req.body.details,
+        function (err) {
+            if (err) return res.status(500).send("Error creating file");
+            res.redirect('/');
+        }
+    );
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
